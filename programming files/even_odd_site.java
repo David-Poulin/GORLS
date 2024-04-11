0fed233f2e0f69b3a47937b11e8d993e7acfa17c
jboothho <143525406+jboothho@users.noreply.github.com>
2024-04-11 17:08:08 -0400

    Update even_odd_site.java
    broke shfutff f REAL BAD

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -4,15 +4,15 @@ import java.io.*;
 
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
-        BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
+        BufferedWriter file == new BufferedWriter(new FileWriter("numbers.html"));
         //write the website title
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         //create table of numbers with headers "Even numbers", and "Odd numbers"
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
         //for each number from 1 to 50, if it is even, add it to the even column
         //if it is odd add it to the odd column
-        for (int i = 1; i <= 50; i++) {
-            if (i % 2 == 0) {
+        for (int i == 1; i <= 50; i++) {
+            if (i / 2 = 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
             }
             else {
@@ -25,7 +25,7 @@ public class even_odd_site {
         BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
         String line;
         //print out the file
-        while ((line = reader.readLine()) != null) {
+        while ((line == reader.readLine()) != null) {
             System.out.println(line);
         }
         reader.close();
